b86a6d3b1bee65b6ab99c53b0a8ee26a933b7b51
Ons <onstounsi2510@gmail.com>
2025-06-29 17:27:02 -0400

    message

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -6,3 +6,12 @@ function calculateArea() {
  let area = length * width;
   document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+function groceryTracker() {
+    let item1 = parseFloat(document.getElementById("grocery1").value) || 0;
+    let item2 = parseFloat(document.getElementById("grocery2").value) || 0;
+    let item3 = parseFloat(document.getElementById("grocery3").value) || 0;
+  
+    let total = item1 + item2 + item3;
+  
+    document.getElementById("totalResult").innerText = `Total des achats d'épicerie : ${total.toFixed(2)} €`;
+  }
